f35912e7b61b13f919e57d19c1704a547b423454
LuxOrionCoding <gungryan1234@gmail.com>
2025-03-22 18:10:43 +0800

    Fixing bug

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,11 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ? "$title - " : '' }} {{ env('APP_NAME') }}</title>
+<title>
+    @if (isset($title))
+        {{ "$title - " }}
+    @endif {{ env('APP_NAME') }}
+</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
