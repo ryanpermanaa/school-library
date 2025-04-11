3915b31503431968d7e1c5763bcfaed4ff9fb33b
LuxOrionCoding <gungryan1234@gmail.com>
2025-04-11 08:51:51 +0800

    ✏️ Forgot password page translate

--- resources/views/livewire/auth/forgot-password.blade.php
+++ resources/views/livewire/auth/forgot-password.blade.php
@@ -1,25 +1,19 @@
  <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Forgot password')" :description="__('Enter your email to receive a password reset link')" />
+     <x-auth-header :title="__('Lupa Password')" :description="__('Masukkan email Anda untuk menerima tautan pengaturan ulang kata sandi')" />
 
-    <!-- Session Status -->
-    <x-auth-session-status class="text-center" :status="session('status')" />
+     <!-- Session Status -->
+     <x-auth-session-status class="text-center" :status="session('status')" />
 
-    <form wire:submit="sendPasswordResetLink" class="flex flex-col gap-6">
-        <!-- Email Address -->
-        <flux:input
-            wire:model="email"
-            :label="__('Email Address')"
-            type="email"
-            required
-            autofocus
-            placeholder="email@example.com"
-        />
+     <form wire:submit="sendPasswordResetLink" class="flex flex-col gap-6">
+         <!-- Email Address -->
+         <flux:input wire:model="email" :label="__('Email')" type="email" required autofocus
+             placeholder="email@example.com" />
 
-        <flux:button variant="primary" type="submit" class="w-full">{{ __('Email password reset link') }}</flux:button>
-    </form>
+         <flux:button variant="primary" type="submit" class="w-full">{{ __('Kirim link reset password') }}</flux:button>
+     </form>
 
-    <div class="space-x-1 text-center text-sm text-zinc-400">
-        {{ __('Or, return to') }}
-        <flux:link :href="route('login')" wire:navigate>{{ __('log in') }}</flux:link>
-    </div>
-</div>
+     <div class="space-x-1 text-center text-sm text-zinc-400">
+         {{ __('Atau kembali ke') }}
+         <flux:link :href="route('login')" wire:navigate>{{ __('log in') }}</flux:link>
+     </div>
+ </div>
